2b63ba9db654f1ec06dc952b28fa75ffc54874b0
Geek2017 <jmlgrefaldo@bicol-u.edu.ph>
2021-11-11 08:06:01 +0800

    almost there

--- SmS/public/controller/billingCtrl.js
+++ SmS/public/controller/billingCtrl.js
@@ -130,6 +130,150 @@ angular.module('newApp').controller('billingCtrl', function($scope, $http, $filt
         console.log('going to page ' + num);
     };
 
+    $scope.repairorder = function(joborder) {
+        $('#repairorder').modal('toggle')
+        firebase.database().ref('/estimate/').orderByChild('ekey').equalTo(joborder.key).on("value", function(snapshot) {
+            let returnArr = [];
+            $timeout(function() {
+                $scope.$apply(function() {
+                    snapshot.forEach(childSnapshot => {
+                        let item = childSnapshot.val();
+                        item.key = childSnapshot.key;
+                        returnArr.push(item);
+                        console.log(item.key)
+                    });
 
+                    $scope.email = returnArr[0].email;
+                    $scope.mobile = returnArr[0].mobileno
+
+                    $scope.grandtotal = returnArr[0].total
+
+                    var num = $scope.grandtotal.replace(/₱ /g, '');
+
+                    var num2 = num.replace(/,/g, '');
+
+                    var num3 = num2 * 0.12;
+
+                    $scope.downpayment = parseFloat(num2) * 0.10;
+
+                    $scope.ngrandtotal = num2;
+
+                    $scope.taxntodal = parseInt(num2) + parseInt(num3)
+
+
+                    console.log(parseInt(num2) + parseInt(num3));
+
+                    console.log(returnArr[0]);
+
+                    if (returnArr[0].details.pmr === 1) {
+                        $('.aa').show();
+                        console.log(returnArr[0].details.pmr);
+                    } else {
+                        $('.aa').hide();
+                    }
+
+                    if (returnArr[0].details.cor === 1) {
+                        $('.bb').show();
+                        console.log(returnArr[0].cor);
+                    } else {
+                        $('.bb').hide();
+                    }
+
+
+                    if (returnArr[0].details.war === 1) {
+                        $('.cc').show();
+                        console.log(returnArr[0].war);
+                    } else {
+                        $('.cc').hide();
+                    }
+
+
+                    if (returnArr[0].details.tsr === 1) {
+                        $('.dd').show();
+                        console.log(returnArr[0].tsr);
+                    } else {
+                        $('.dd').hide();
+                    }
+
+
+                    if (returnArr[0].details.gsr !== 0) {
+                        $('.ee').show();
+                        console.log(returnArr[0].gsr);
+                    } else {
+                        $('.ee').hide();
+                    }
+
+
+                    if (returnArr[0].details.pnmr !== 0) {
+                        $('.ff').show();
+                        console.log(returnArr[0].pnmr);
+                    } else {
+                        $('.ff').hide();
+                    }
+
+                    if (returnArr[0].details.indr !== 0) {
+                        $('.gg').show();
+                        console.log(returnArr[0].indr);
+                    } else {
+                        $('.gg').hide();
+                    }
+
+                    if (returnArr[0].details.others !== 0) {
+                        $('.hh').show();
+                        console.log(returnArr[0].others);
+                    } else {
+                        $('.hh').hide();
+                    }
+
+
+                    $scope.cmake = returnArr[0].details.make;
+                    $scope.cmodel = returnArr[0].details.model;
+                    $scope.transmission = returnArr[0].details.transmission;
+                    $scope.cyear = returnArr[0].details.year;
+                    $scope.cengine = returnArr[0].details.engine;
+                    $scope.ckm = returnArr[0].details.mileage;
+
+                    $('#viewestimate').modal('toggle')
+
+                    $scope.qoutes = returnArr[0];
+                });
+
+            })
+
+        });
+
+        $scope.print = function() {
+            $(".btnhide").hide();
+
+            printElement(document.getElementById("printThis"));
+
+
+
+            function printElement(elem) {
+                var domClone = elem.cloneNode(true);
+
+                var $printSection = document.getElementById("printSection");
+
+                if (!$printSection) {
+                    var $printSection = document.createElement("div");
+                    $printSection.id = "printSection";
+                    document.body.appendChild($printSection);
+                }
+
+                $printSection.innerHTML = "";
+                $printSection.appendChild(domClone);
+                window.print();
+
+                setTimeout(function() {
+                    $('#repairorder').modal('toggle')
+                    $(".btnhide").show();
+                }, 1000);
+
+            }
+
+        }
+
+
+    }
 
 })
--- SmS/public/controller/jobordersCrtl.js
+++ SmS/public/controller/jobordersCrtl.js
@@ -1152,7 +1152,7 @@ angular.module('newApp').controller('jobordersCrtl', function($scope, $http, $fi
                     .once('value')
                     .then(function(snapshot) {
                         snapshot.forEach(function(childSnapshot) {
-                            childSnapshot.ref.child('stage').set(4);
+                            childSnapshot.ref.child('state').set(4);
                         });
                     });
 
@@ -1197,6 +1197,172 @@ angular.module('newApp').controller('jobordersCrtl', function($scope, $http, $fi
             console.log(response);
         });
 
+    }
+
+    $scope.repairorder = function() {
+        $('#repairorder').modal('toggle')
+        firebase.database().ref('/estimate/').orderByChild('ekey').equalTo(itemkey).on("value", function(snapshot) {
+            let returnArr = [];
+            $timeout(function() {
+                $scope.$apply(function() {
+                    snapshot.forEach(childSnapshot => {
+                        let item = childSnapshot.val();
+                        item.key = childSnapshot.key;
+                        returnArr.push(item);
+                        console.log(item.key)
+                    });
+
+                    $scope.email = returnArr[0].email;
+                    $scope.mobile = returnArr[0].mobileno
+
+                    $scope.grandtotal = returnArr[0].total
+
+                    var num = $scope.grandtotal.replace(/₱ /g, '');
+
+                    var num2 = num.replace(/,/g, '');
+
+                    var num3 = num2 * 0.12;
+
+                    $scope.downpayment = parseFloat(num2) * 0.10;
+
+                    $scope.ngrandtotal = num2;
+
+                    $scope.taxntodal = parseInt(num2) + parseInt(num3)
+
+
+                    console.log(parseInt(num2) + parseInt(num3));
+
+                    console.log(returnArr[0]);
+
+                    if (returnArr[0].details.pmr === 1) {
+                        $('.aa').show();
+                        console.log(returnArr[0].details.pmr);
+                    } else {
+                        $('.aa').hide();
+                    }
+
+                    if (returnArr[0].details.cor === 1) {
+                        $('.bb').show();
+                        console.log(returnArr[0].cor);
+                    } else {
+                        $('.bb').hide();
+                    }
+
+
+                    if (returnArr[0].details.war === 1) {
+                        $('.cc').show();
+                        console.log(returnArr[0].war);
+                    } else {
+                        $('.cc').hide();
+                    }
+
+
+                    if (returnArr[0].details.tsr === 1) {
+                        $('.dd').show();
+                        console.log(returnArr[0].tsr);
+                    } else {
+                        $('.dd').hide();
+                    }
+
+
+                    if (returnArr[0].details.gsr !== 0) {
+                        $('.ee').show();
+                        console.log(returnArr[0].gsr);
+                    } else {
+                        $('.ee').hide();
+                    }
+
+
+                    if (returnArr[0].details.pnmr !== 0) {
+                        $('.ff').show();
+                        console.log(returnArr[0].pnmr);
+                    } else {
+                        $('.ff').hide();
+                    }
+
+                    if (returnArr[0].details.indr !== 0) {
+                        $('.gg').show();
+                        console.log(returnArr[0].indr);
+                    } else {
+                        $('.gg').hide();
+                    }
+
+                    if (returnArr[0].details.others !== 0) {
+                        $('.hh').show();
+                        console.log(returnArr[0].others);
+                    } else {
+                        $('.hh').hide();
+                    }
+
+
+                    $scope.cmake = returnArr[0].details.make;
+                    $scope.cmodel = returnArr[0].details.model;
+                    $scope.transmission = returnArr[0].details.transmission;
+                    $scope.cyear = returnArr[0].details.year;
+                    $scope.cengine = returnArr[0].details.engine;
+                    $scope.ckm = returnArr[0].details.mileage;
+
+                    $('#viewestimate').modal('toggle')
+
+                    $scope.qoutes = returnArr[0];
+                });
+
+            })
+
+        });
+
+        $scope.print = function() {
+            $(".btnhide").hide();
+
+            printElement(document.getElementById("printThis"));
+
+
+
+            function printElement(elem) {
+                var domClone = elem.cloneNode(true);
+
+                var $printSection = document.getElementById("printSection");
+
+                if (!$printSection) {
+                    var $printSection = document.createElement("div");
+                    $printSection.id = "printSection";
+                    document.body.appendChild($printSection);
+                }
+
+                $printSection.innerHTML = "";
+                $printSection.appendChild(domClone);
+                window.print();
+
+                setTimeout(function() {
+                    $('#repairorder').modal('toggle')
+                }, 1000);
+
+            }
+
+        }
+
+
+    }
+
+    $scope.completejo = function() {
+        $('.sucnotif').show();
+
+        var db = firebase.database();
+        db.ref('/joborders/')
+            .orderByChild("ekey")
+            .equalTo(itemkey)
+            .once('value')
+            .then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                    childSnapshot.ref.child('state').set(6);
+                });
+            });
+
+        setTimeout(function() {
+            $('#viewestimate').modal('hide');
+            location.replace('#/')
+            location.replace('#/billing')
+        }, 3000);
     }
 
 })
